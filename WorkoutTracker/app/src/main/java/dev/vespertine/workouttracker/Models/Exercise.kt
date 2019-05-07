d4839b5de0fe91dd89ec462719ef50927ebb8942
Patrick Martin <patrickmartin88@gmail.com>
2019-05-07 13:37:57 -0700

    Implement Foreign Keys to attach exercise to routine.

--- WorkoutTracker/app/src/main/java/dev/vespertine/workouttracker/Models/Exercise.kt
+++ WorkoutTracker/app/src/main/java/dev/vespertine/workouttracker/Models/Exercise.kt
@@ -1,10 +1,25 @@
 package dev.vespertine.workouttracker.Models
 
-data class Exercise(val name: String,
-                    var isMachine: Boolean,
-                    var weight: Double,
-                    var sets: Int,
-                    var repRange: Int,
-                    var mapReps: Int,
-                    var mapSets: Int,
-                    var failed: Boolean)
+import android.arch.persistence.room.*
+
+@Entity(tableName = "exercises",
+    indices = [(Index(value = ["exercise_id"], name = "idx_exercises_exercise_id"))],
+    foreignKeys =
+    [(ForeignKey(
+        entity = Exercise::class,
+        parentColumns = ["worokoutroutine_id"],
+        childColumns = ["exercise_id"],
+        onUpdate = ForeignKey.CASCADE,
+        onDelete = ForeignKey.CASCADE))])
+data class Exercise( @PrimaryKey(autoGenerate = true)
+                     @ColumnInfo(name = "exercise_id")
+                     val workoutroutineID: Long,
+                     val exerciseID: Long,
+                     val name: String,
+                     var isMachine: Boolean,
+                     var weight: Double,
+                     var sets: Int,
+                     var repRange: Int,
+                     var mapReps: Int,
+                     var mapSets: Int,
+                     var failed: Boolean)
--- WorkoutTracker/app/src/main/java/dev/vespertine/workouttracker/Models/WorkoutRoutine.kt
+++ WorkoutTracker/app/src/main/java/dev/vespertine/workouttracker/Models/WorkoutRoutine.kt
@@ -1,3 +1,15 @@
 package dev.vespertine.workouttracker.Models
 
-data class WorkoutRoutine(val dayNumber: Int, val routine: List<Exercise>)
+import android.arch.persistence.room.ColumnInfo
+import android.arch.persistence.room.Entity
+import android.arch.persistence.room.PrimaryKey
+
+@Entity(tableName = "workoutroutines")
+data class WorkoutRoutine(@PrimaryKey(autoGenerate = true)
+                          @ColumnInfo(name = "worokoutroutine_id")
+                          val workoutroutineID: Long,
+                          @ColumnInfo(name = "routine_name")
+                          val routineName: String,
+                          @ColumnInfo(name = "day_number")
+                          val dayNumber: Int,
+                          val tags: String)
